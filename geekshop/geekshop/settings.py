5450e9cbdaefc262d845a2514f5cf2ed784652db
Alexander <alexzy_coder@mail.ru>
2022-02-08 07:40:23 +0300

    second lesson is done

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -56,7 +56,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [],
-        'APP_DIRS': True, # ищет шаблоны в папках mainapp
+        'APP_DIRS': True,  # ищет шаблоны в папках mainapp
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
@@ -121,7 +121,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / 'static',
 ]
 
 # Default primary key field type
--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,15 +1,36 @@
+import json
 from django.shortcuts import render
 
 # Create your views here.
 
+MENU_LINKS = [
+    {'url': 'index', 'name': 'домой'},
+    {'url': 'products', 'name': 'продукты'},
+    {'url': 'contact', 'name': 'контакты'},
+]
+
 
 def index(request):
-    return render(request, 'mainapp/index.html')
+    return render(request, 'mainapp/index.html', context={
+        'title': 'главная',
+        'menu_links': MENU_LINKS,
+
+    })
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    with open('./menu_product.json', 'r') as file:
+        menu_product = json.load(file)
+    return render(request, 'mainapp/products.html', context={
+        'title': 'продукты',
+        'menu_links': MENU_LINKS,
+        'menu_product': menu_product,
+    })
 
 
 def contact(request):
-    return render(request, 'mainapp/contact.html')
+
+    return render(request, 'mainapp/contact.html', context={
+        'title': 'контакты',
+        'menu_links': MENU_LINKS,
+    })
