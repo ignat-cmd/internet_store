4b1f221ce7b04ce980b22d53c727af1e987e390f
Alexander <alexzy_coder@mail.ru>
2022-04-10 10:18:58 +0300

    users view changed on CBV

--- geekshop/adminapp/views/user.py
+++ geekshop/adminapp/views/user.py
@@ -1,11 +1,13 @@
 from django.http.response import HttpResponseRedirect
 from turtle import title
 from django.shortcuts import render, get_object_or_404
-from authapp.forms import ShopUserEditForm
+from authapp.forms import ShopUserEditForm, ShopUserRegisterForm
 from authapp.models import ShopUser
 from adminapp.utils import superuser_requared
 from adminapp.forms import ShopUserAdminForm
 from django.urls import reverse
+from django.views.generic.list import ListView
+from django.utils.decorators import method_decorator
 
 
 @superuser_requared
@@ -13,13 +15,13 @@ def user_create(request):
     title = 'Создание пользователя'
 
     if request.method == 'POST':
-        edit_form = ShopUserAdminForm(
+        edit_form = ShopUserRegisterForm(
             request.POST, request.FILES)
         if edit_form.is_valid():
             edit_form.save()
             return HttpResponseRedirect(reverse('admin:users'))
     else:
-        edit_form = ShopUserAdminForm()
+        edit_form = ShopUserRegisterForm()
 
     return render(request, 'adminapp/user/edit.html', context={
         'form': edit_form,
