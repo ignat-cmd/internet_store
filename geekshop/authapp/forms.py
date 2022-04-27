65eb6af4f345b1cd42c8bbd3f33e510470967a95
Alexander <alexzy_coder@mail.ru>
2022-04-27 22:12:27 +0300

    autentification_via_socialnet

--- geekshop/authapp/forms.py
+++ geekshop/authapp/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm
-from .models import ShopUser
+from .models import ShopUser, ShopUserProfile
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.forms import UserChangeForm
 import random, hashlib
@@ -73,3 +73,14 @@ class ShopUserEditForm(UserChangeForm):
         if data < 18:
             raise forms.ValidationError("Вы слишком молоды!")
         return data
+
+class ShopUserProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = ShopUserProfile
+        fields = ('tagline', 'aboutMe', 'gender')
+
+
+    def __init__(self, *args, **kwargs):
+        super(ShopUserProfileEditForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
--- geekshop/authapp/models.py
+++ geekshop/authapp/models.py
@@ -3,6 +3,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from datetime import timedelta
 from django.utils.timezone import now
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 
@@ -12,7 +14,7 @@ def get_expire_reg_key():
     return now() + timedelta(hours=48)
 
 class ShopUser(AbstractUser):
-    age = models.PositiveBigIntegerField(verbose_name='возраст')
+    age = models.PositiveBigIntegerField(verbose_name='возраст', default=18)
     avatar = models.ImageField(
         verbose_name='аватар', upload_to='users_avatars', blank=True)
 
@@ -24,3 +26,31 @@ class ShopUser(AbstractUser):
             return False
         else:
             return True
+
+class ShopUserProfile(models.Model):
+    MALE = 'M'
+    FEMALE = 'W'
+    
+    GENDER_CHOICES = (
+    (MALE, 'М'),
+    (FEMALE, 'Ж'),
+    )
+
+    user = models.OneToOneField(ShopUser, unique=True, null=False,\
+                                db_index=True, on_delete=models.CASCADE)
+    tagline = models.CharField(verbose_name='теги', max_length=128, \
+                                blank=True)
+    aboutMe = models.TextField(verbose_name='о себе', max_length=512, \
+                                blank=True)
+    gender = models.CharField(verbose_name='пол', max_length=1, \
+                            choices=GENDER_CHOICES, blank=True)
+
+    @receiver(post_save, sender=ShopUser)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            ShopUserProfile.objects.create(user=instance)
+
+
+    @receiver(post_save, sender=ShopUser)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.shopuserprofile.save()
